e97f44f05e649141831fd363e5c015d12014ca90
Erza0838 <muhammaderza0838@gmail.com>
2023-07-08 19:35:51 +0700

    Belajar c# list collection

---
+++ Array_List_Exercise.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+
+class  Array_List_Exercise
+{
+    public static void Main()
+    {
+        // Create and initializes a new ArrayList
+        List<int> StudentsProfileList = new List<int>();
+        // ArrayList myAL = new ArrayList();
+    }
+}
